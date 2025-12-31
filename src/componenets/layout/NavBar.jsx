b482726d6708ee8ee7047e9eaafe013034fa4eea
puspitasahu <puspita5sahu@gmail.com>
2025-12-31 11:17:34 +0530

    added Navbar componenet

--- src/componenets/layout/NavBar.jsx
+++ src/componenets/layout/NavBar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import useLocation from 'react-router-dom'
 
 const NavBar = () => {
+  const location = useLocation();
+  const [activeItem, setActiveItem] = React.useState(location.pathname);
+
+  const handleItemClick = (path) => {
+    setActiveItem(path);
+  }
+
+  const navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/chat-support', label: 'Chat' },
+    { path: '/ticket', label: 'Tickets' }
+  ]
+
   return (
-    <div>NavBar</div>
+    <nav className='nav-container'>
+      <ul className='nav-list'>
+        {
+          navItems.map((item) => {
+            <li key={item.path} className="nav-item" >
+
+              <link to={item.path} className={activeItem == item.path ? "active" : ""} onClick={handleItemClick(item.path)}>
+                {item.label}
+              </link>
+
+            </li>
+
+          })
+        }
+      </ul>
+    </nav> >
   )
 }
 
