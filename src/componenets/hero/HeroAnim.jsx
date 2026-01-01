aff2c6892300f9405897998c32d54ee2ac054ab5
puspitasahu <puspita5sahu@gmail.com>
2026-01-01 10:03:07 +0530

    added SupportTream Component

--- src/componenets/hero/HeroAnim.jsx
+++ src/componenets/hero/HeroAnim.jsx
@@ -5,18 +5,18 @@ import s3 from "../../assets/images/support3.png";
 import s4 from "../../assets/images/support4.png";
 import s5 from "../../assets/images/support5.png";
 
-const images = [s1, s2, s3, s4, s5];
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 15000,
-  fade: true,
-  autoplay: true,
-  autoplayspeed: 3000,
-};
-
 const HeroAnim = () => {
+  const images = [s1, s2, s3, s4, s5];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 15000,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 3000,
+  };
+
   return (
     <div className="slider-container">
       <Slider {...settings}>
--- src/componenets/team/SupportTeam.jsx
+++ src/componenets/team/SupportTeam.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { supportTeam } from "./SupportTeam";
 const SupportTeam = () => {
-  return <div>Support Team</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 10000,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 3000,
+  };
+  return (
+    <section className="team-section mb-5" id="support">
+      <h2 className="team-title">Meet Our Support Team</h2>
+      <Slider {...settings}>
+        {supportTeam.map((member) => {
+          <div key={member.name} className="team-member">
+            <img src={member.img} alt={member.name} />
+            <div>
+              <span className="member-name">{member.name}</span>
+              <br />
+              <span className="member-role">{member.role}</span>
+            </div>
+          </div>;
+        })}
+      </Slider>
+    </section>
+  );
 };
 
 export default SupportTeam;
