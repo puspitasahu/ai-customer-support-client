9118808c14cef13cb85edb638a5e88269ce94b61
puspitasahu <puspita5sahu@gmail.com>
2026-01-03 11:41:09 +0530

    added chat component

--- src/componenets/chat/Chat.jsx
+++ src/componenets/chat/Chat.jsx
@@ -11,7 +11,7 @@ const Chat = () => {
   /* Use Stomp WebSocket Hook to recive mesages asychronously from AI (Assistamt) */
   const { messages: wsMessages } = useStompWebSocket(sessionId);
 
-  /*Update messages when new messages are recived from websocket*/
+  /*Generate a session or load sessionId when component mounts */
   useEffect(() => {
     let chatId = localStorage.getItem("sessionId");
     if (!chatId) {
@@ -21,6 +21,50 @@ const Chat = () => {
     setSessionId(chatId);
   }, []);
 
+  //combine local messages with the messages from websoket
+
+  useEffect(() => {
+    if (!wsMessages) return;
+
+    //fliter out the messages that are already in local messages
+
+    setMessage((prev) => {
+        const nwmsgs = wsMessages.filter(
+            (wsMsg) =>
+                !prev.some(
+                    (msg) => msg.content == wsMsg.content && msg.role == wsMsg.role
+                )
+        );
+        if(nwmsgs.length >0)
+            setWaitingForAI(false);
+
+        return[...prev,...nwmsgs]
+    }
+
+  },[wsMessages])
+
+  const Chat = () =>{
+    const chatWithAI = async () =>{
+        setMessages((prev) =>
+        [...prev,{role:"user",contentmessage}])
+        setWaitingForAI(true);
+        try{
+            const response  =await sendmessage(sessionId.message);
+            setMessage((prev)=>[
+                [...prev,{role :"assistant",content : response}]
+            ]);
+        setWaitingForAI(false);
+        }catch(error){
+            console.error("Error sendimg message",error);
+            setMessage((prev) =>[
+                [...prev,{role :"assistant",content : "Error sending message"}]
+            ]);
+        setWaitingForAI(false);
+
+        }
+        setMessage("");
+    }
+  }
   return <div>Chat</div>;
 };
 
